a94d43ee522db62cc1af6eeba555842865798e36
KshitijKoli <kshitijparmeshwar73@gmail.com>
2026-02-05 19:59:10 +0530

    Update OOP5_Method_Overriding.java

--- OOP5_Method_Overriding.java
+++ OOP5_Method_Overriding.java
@@ -1,5 +1,10 @@
 // Method Overriding : When child has same properties as their parents then the child overrides the parent 
 // meaning instead of getting parents property, child uses its own property
+// Method Overriding is also known as Runtime Polymorphism
+// Polymorphism means “many forms.” In Java, it means one method or object behaves differently in different situations.
+// Types of Polymorphism in Java :-
+// 1)Compile-time Polymorphism (Method Overloading)
+// 2)Runtime Polymorphism (Method Overriding)
 class Vehicle{
     void start(){
       System.out.println("Vehicle will Start");
