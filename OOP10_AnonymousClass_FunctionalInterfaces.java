2c3b8077e79322050d827450dd6d04504bd2c0cb
KshitijKoli <kshitijparmeshwar73@gmail.com>
2026-02-13 10:52:27 +0530

    Create OOP10_AnonymousClass_FunctionalInterfaces.java

---
+++ OOP10_AnonymousClass_FunctionalInterfaces.java
@@ -0,0 +1,41 @@
+// In java we can create an class inside an class i.e nested class, we can also create an class without name inside an class i.e Anonymous class
+// Anonymous class usually extend subclass or implements interfaces
+// 1) A superclass that an anonymous class extends
+public class Anonymousclass{
+  Outerclass obj=new Outerclass(){      //instead of extending Outerclass we created Anonymous class. Obj of the class is created, It can be used only one time 
+
+  };
+class Outerclass{
+  public void outerMethod(){
+
+  }
+}
+  
+// 2) An Interface that an anonymous class implements.
+public class Anonymousclass{
+  SuperInterface obj=new SuperInterface(){
+    
+  @override
+  public void interfaceMethod(){
+      
+    }
+  };
+innterface SuperInterface{
+    void interfaceMethod(){
+      }
+}
+
+// Functional Interfaces:
+// An Interface that contains exactly one abstract method is known as a functional interface 
+// Functional Interfaces introduced in java 8 allows us to use a lambda expression 
+  public class Anonymousclass{
+   Sample obj= (int val)->{
+     S.o.p(val);
+       return val;
+      };
+  }
+  @FunctionalInterface 
+  interface Sample{
+    //abstract method
+    int calculate(int val);
+  }
